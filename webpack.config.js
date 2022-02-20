4e171c39a51c11eb8bff5ed929502c394daab4c5
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-20 15:41:50 +0200

    Switch to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  // mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',

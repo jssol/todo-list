51f4efc1f120d4c7edc6ec036acf8bfd99d220a1
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-23 15:16:11 +0200

    Fix linters

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -1,138 +1,138 @@
-import Task from "./task.js";
-import TaskStatus from "./taskStatus.js";
+import Task from './task.js';
+import TaskStatus from './taskStatus.js';
 
-const tasksList = document.querySelector(".tasks-list");
+const tasksList = document.querySelector('.tasks-list');
 const taskStatus = new TaskStatus();
 
 export default class TaskList {
-	constructor() {
-		this.tasks = [];
-	}
+  constructor() {
+    this.tasks = [];
+  }
 
 	setStore = () => {
-		localStorage.setItem("todo-list", JSON.stringify(this.tasks));
+	  localStorage.setItem('todo-list', JSON.stringify(this.tasks));
 	};
 
 	getStore = () => {
-		if (localStorage.getItem("todo-list")) {
-			this.tasks = JSON.parse(localStorage.getItem("todo-list"));
-		}
+	  if (localStorage.getItem('todo-list')) {
+	    this.tasks = JSON.parse(localStorage.getItem('todo-list'));
+	  }
 	};
 
 	removeTask = (index) => {
-		this.tasks.splice(index, 1);
+	  this.tasks.splice(index, 1);
 	};
 
 	displayTasks = (taskslist) => {
-		taskslist.innerHTML = "";
-		this.tasks.forEach((task, index) => {
-			const li = document.createElement("li");
-			const check = document.createElement("button");
-			const dots = document.createElement("button");
-			const del = document.createElement("button");
-			const field = document.createElement("p");
-			const inputField = document.createElement("input");
-			li.id = task.index;
-			li.className = "task";
-			field.textContent = task.description;
-			inputField.value = task.description;
-			field.className = "task-description";
-			inputField.className = "task-mod";
-			dots.classList.add("button", "dots");
-			del.classList.add("button", "del");
-			check.classList.add("button", "check");
-			dots.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	  taskslist.innerHTML = '';
+	  this.tasks.forEach((task, index) => {
+	    const li = document.createElement('li');
+	    const check = document.createElement('button');
+	    const dots = document.createElement('button');
+	    const del = document.createElement('button');
+	    const field = document.createElement('p');
+	    const inputField = document.createElement('input');
+	    li.id = task.index;
+	    li.className = 'task';
+	    field.textContent = task.description;
+	    inputField.value = task.description;
+	    field.className = 'task-description';
+	    inputField.className = 'task-mod';
+	    dots.classList.add('button', 'dots');
+	    del.classList.add('button', 'del');
+	    check.classList.add('button', 'check');
+	    dots.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
       </svg>`;
-			del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	    del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
       </svg>`;
-			check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	    check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
       </svg>`;
 
-			if (task.completed) {
-				li.classList.add("completed");
-				check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	    if (task.completed) {
+	      li.classList.add('completed');
+	      check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
         </svg>`;
-			}
+	    }
 
-			check.addEventListener("click", () => {
-				li.classList.toggle("completed");
-				if (li.classList.contains("completed")) {
-					check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	    check.addEventListener('click', () => {
+	      li.classList.toggle('completed');
+	      if (li.classList.contains('completed')) {
+	        check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
           </svg>`;
-					task.completed = taskStatus.updateStatus(li);
-					this.setStore();
-				} else {
-					check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+	        task.completed = taskStatus.updateStatus(li);
+	        this.setStore();
+	      } else {
+	        check.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
           </svg>`;
-					task.completed = taskStatus.updateStatus(li);
-					this.setStore();
-				}
-			});
+	        task.completed = taskStatus.updateStatus(li);
+	        this.setStore();
+	      }
+	    });
 
-			dots.addEventListener("click", (e) => {
-				e.stopPropagation();
-				li.classList.add("edit");
-				inputField.focus();
+	    dots.addEventListener('click', (e) => {
+	      e.stopPropagation();
+	      li.classList.add('edit');
+	      inputField.focus();
 
-				if (li.classList.contains("edit")) {
-					li.addEventListener("keydown", (e) => {
-						if (e.key === "Enter" && inputField.value.trim()) {
-							task.description = inputField.value.trim();
-							this.setStore();
-							this.displayTasks(tasksList);
-							inputField.blur();
-							li.classList.remove("edit");
-						}
-					});
-				}
-			});
+	      if (li.classList.contains('edit')) {
+	        li.addEventListener('keydown', (e) => {
+	          if (e.key === 'Enter' && inputField.value.trim()) {
+	            task.description = inputField.value.trim();
+	            this.setStore();
+	            this.displayTasks(tasksList);
+	            inputField.blur();
+	            li.classList.remove('edit');
+	          }
+	        });
+	      }
+	    });
 
-			inputField.addEventListener("mouseout", (e) => {
-				e.stopPropagation();
-				if (inputField.value.trim()) {
-					task.description = inputField.value.trim();
-					this.setStore();
-					this.displayTasks(tasksList);
-				}
-				li.classList.remove("edit");
-			});
+	    inputField.addEventListener('mouseout', (e) => {
+	      e.stopPropagation();
+	      if (inputField.value.trim()) {
+	        task.description = inputField.value.trim();
+	        this.setStore();
+	        this.displayTasks(tasksList);
+	      }
+	      li.classList.remove('edit');
+	    });
 
-			del.addEventListener("click", (e) => {
-				e.stopPropagation();
-				this.removeTask(index);
-				this.tasks.forEach((task, index) => {
-					task.index = index + 1;
-				});
-				this.setStore();
-				this.displayTasks(tasksList);
-			});
+	    del.addEventListener('click', (e) => {
+	      e.stopPropagation();
+	      this.removeTask(index);
+	      this.tasks.forEach((task, index) => {
+	        task.index = index + 1;
+	      });
+	      this.setStore();
+	      this.displayTasks(tasksList);
+	    });
 
-			li.append(check, field, inputField, del, dots);
-			taskslist.appendChild(li);
-		});
+	    li.append(check, field, inputField, del, dots);
+	    taskslist.appendChild(li);
+	  });
 	};
 
 	addTask = (description) => {
-		if (description.trim()) {
-			const task = new Task(description.trim(), this.tasks);
-			this.tasks.push(task);
-		}
+	  if (description.trim()) {
+	    const task = new Task(description.trim(), this.tasks);
+	    this.tasks.push(task);
+	  }
 	};
 
 	clearAll = () => {
-		this.tasks = [];
+	  this.tasks = [];
 	};
 
 	clearCompleted = () => {
-		this.tasks = this.tasks.filter((task) => task.completed === false);
-		this.tasks.forEach((task, index) => {
-			task.index = index + 1;
-		});
+	  this.tasks = this.tasks.filter((task) => task.completed === false);
+	  this.tasks.forEach((task, index) => {
+	    task.index = index + 1;
+	  });
 	};
 }
--- src/modules/taskManager.test.js
+++ src/modules/taskManager.test.js
@@ -2,53 +2,35 @@
  * @jest-environment jsdom
  */
 
-import TaskList from "./taskManager";
-import Task from "./task";
+import TaskList from './taskManager.js';
 
 const taskClass = new TaskList();
 
-// describe("testing adding and deleting", () => {
-// 	test("adding", () => {
-// 		expect(taskClass.addTask("Hello")).toMatch(taskClass.tasks[0]);
-// 	});
-// });
-describe("testing adding ", () => {
-	test("adding the task", () => {
-		taskClass.addTask("Hello");
-		expect(taskClass.tasks[0].description).toEqual("Hello");
-	});
-	test("adding the task", () => {
-		taskClass.addTask("world");
-		expect(taskClass.tasks[1].description).toEqual("world");
-	});
-	test("adding the task", () => {
-		taskClass.addTask("Microverse");
-		expect(taskClass.tasks[2].description).toEqual("Microverse");
-	});
+describe('testing adding ', () => {
+  test('adding the task', () => {
+    taskClass.addTask('Hello');
+    expect(taskClass.tasks[0].description).toEqual('Hello');
+  });
+  test('adding the task', () => {
+    taskClass.addTask('world');
+    expect(taskClass.tasks[1].description).toEqual('world');
+  });
+  test('adding the task', () => {
+    taskClass.addTask('Microverse');
+    expect(taskClass.tasks[2].description).toEqual('Microverse');
+  });
 });
-describe("testing deleting ", () => {
-	test("deleting the task", () => {
-		taskClass.removeTask(0);
-		expect(taskClass.tasks.length).toBe(2);
-	});
-	test("deleting the task", () => {
-		taskClass.removeTask(1);
-		expect(taskClass.tasks.length).toBe(1);
-	});
-	test("deleting the task", () => {
-		taskClass.removeTask(0);
-		expect(taskClass.tasks.length).toBe(0);
-	});
+describe('testing deleting ', () => {
+  test('deleting the task', () => {
+    taskClass.removeTask(0);
+    expect(taskClass.tasks.length).toBe(2);
+  });
+  test('deleting the task', () => {
+    taskClass.removeTask(1);
+    expect(taskClass.tasks.length).toBe(1);
+  });
+  test('deleting the task', () => {
+    taskClass.removeTask(0);
+    expect(taskClass.tasks.length).toBe(0);
+  });
 });
-
-// addTask = (description) => {
-// 	if (description.trim()) {
-// 		const task = new Task(description.trim(), this.tasks);
-// 		this.tasks.push(task);
-// 	}
-// };
-
-// test("the shopping list has milk on it", () => {
-// 	expect(shoppingList).toContain("milk");
-// 	expect(new Set(shoppingList)).toContain("milk");
-// });

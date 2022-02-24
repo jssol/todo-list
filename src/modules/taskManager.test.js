dfc9d459979108f6576f5fe4cffcb95a9467a0ad
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-02-24 16:30:43 +0530

    Added updating dom on editing

--- src/modules/taskManager.test.js
+++ src/modules/taskManager.test.js
@@ -2,96 +2,93 @@
  * @jest-environment jsdom
  */
 
- import TaskList from './taskManager.js';
+import TaskList from './taskManager.js';
 
- document.body.innerHTML = '<div>'
-       + '  <ul id="list"></ul>'
-       + '</div>';
- const itemsList = document.querySelector('#list');
- 
- const taskClass = new TaskList();
- 
- describe('testing adding ', () => {
-   test('adding the task', () => {
-     taskClass.addTask('Hello');
-     expect(taskClass.tasks[0].description).toEqual('Hello');
-   });
-   test('adding the task', () => {
-     taskClass.addTask('world');
-     expect(taskClass.tasks[1].description).toEqual('world');
-   });
-   test('adding the task', () => {
-     taskClass.addTask('Microverse');
-     expect(taskClass.tasks[2].description).toEqual('Microverse');
-   });
- });
- 
- describe('testing deleting ', () => {
-   test('deleting the task', () => {
-     taskClass.removeTask(0);
-     expect(taskClass.tasks.length).toBe(2);
-   });
-   test('deleting the task', () => {
-     taskClass.removeTask(1);
-     expect(taskClass.tasks.length).toBe(1);
-   });
-   test('deleting the task', () => {
-     taskClass.removeTask(0);
-     expect(taskClass.tasks.length).toBe(0);
-   });
- });
- 
- describe('adding, deleting or editing li items to the ul in the DOM', () => {
-   describe('adding items to the list', () => {
-     test('Add Hello to the list', () => {
-       taskClass.addTask('Hello');
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
-       expect(list).toHaveLength(1);
-     });
-     test('Add World to the list', () => {
-       taskClass.addTask('World');
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
-       expect(list).toHaveLength(2);
-     });
-   });
- 
-   describe('deleting items from the list', () => {
-     test('Remove Hello from the list', () => {
-       taskClass.removeTask(0);
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
-       expect(list).toHaveLength(1);
-     });
-     test('Remove World from the list', () => {
-       taskClass.removeTask(0);
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
-       expect(list).toHaveLength(0);
-     });
-   });
- 
-   describe('updating items on the list', () => {
-     test('change Hello to Hell on the list', () => {
-       taskClass.addTask('Hello');
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
- 
-       taskClass.tasks[0].description = 'Hell';
-       taskClass.displayTasks(itemsList);
-       expect(list[0].textContent).toMatch('Hell');
-     });
- 
-     test('change World to Metaverse on the list', () => {
-       taskClass.addTask('World');
-       taskClass.displayTasks(itemsList);
-       const list = document.querySelectorAll('#list li');
- 
-       taskClass.tasks[1].description = 'Metaverse';
-       taskClass.displayTasks(itemsList);
-       expect(list[1].textContent).toMatch('Metaverse');
-     });
-   });
- });
- 
+document.body.innerHTML = '<div>' +
+	'  <ul id="list"></ul>' +
+	'</div>';
+const itemsList = document.querySelector('#list');
+
+const taskClass = new TaskList();
+
+describe('testing adding ', () => {
+	test('adding the task', () => {
+		taskClass.addTask('Hello');
+		expect(taskClass.tasks[0].description).toEqual('Hello');
+	});
+	test('adding the task', () => {
+		taskClass.addTask('world');
+		expect(taskClass.tasks[1].description).toEqual('world');
+	});
+	test('adding the task', () => {
+		taskClass.addTask('Microverse');
+		expect(taskClass.tasks[2].description).toEqual('Microverse');
+	});
+});
+
+describe('testing deleting ', () => {
+	test('deleting the task', () => {
+		taskClass.removeTask(0);
+		expect(taskClass.tasks.length).toBe(2);
+	});
+	test('deleting the task', () => {
+		taskClass.removeTask(1);
+		expect(taskClass.tasks.length).toBe(1);
+	});
+	test('deleting the task', () => {
+		taskClass.removeTask(0);
+		expect(taskClass.tasks.length).toBe(0);
+	});
+});
+
+describe('adding, deleting or editing li items to the ul in the DOM', () => {
+	describe('adding items to the list', () => {
+		test('Add Hello to the list', () => {
+			taskClass.addTask('Hello');
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			expect(list).toHaveLength(1);
+		});
+		test('Add World to the list', () => {
+			taskClass.addTask('World');
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			expect(list).toHaveLength(2);
+		});
+	});
+
+	describe('deleting items from the list', () => {
+		test('Remove Hello from the list', () => {
+			taskClass.removeTask(0);
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			expect(list).toHaveLength(1);
+		});
+		test('Remove World from the list', () => {
+			taskClass.removeTask(0);
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			expect(list).toHaveLength(0);
+		});
+	});
+
+	describe('updating items on the list', () => {
+		test('change Hello World to Hello on the list', () => {
+			taskClass.addTask('Hello World');
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			taskClass.tasks[0].description = 'Hello';
+			taskClass.displayTasks(itemsList);
+			expect(list[0].textContent).toMatch('Hello');
+		});
+
+		test('change Microverse the best to the best on the list', () => {
+			taskClass.addTask('Microverse the best');
+			taskClass.displayTasks(itemsList);
+			const list = document.querySelectorAll('#list li');
+			taskClass.tasks[1].description = 'the best';
+			taskClass.displayTasks(itemsList);
+			expect(list[1].textContent).toMatch('the best');
+		});
+	});
+});
